065c4645be1b92e570fa71133f082325b44d34a5
MbongeniCloud <Lefotlhem@yahoo.com>
2026-04-19 06:23:14 -0700

    Restore all variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,27 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-securefiles"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "southafricanorth"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+  default     = "stsecurefiles001"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
 variable "client_id" {
   description = "Azure Client ID"
   type        = string
